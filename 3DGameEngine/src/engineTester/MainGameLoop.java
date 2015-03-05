b2170ff0405ff89ffe74b7f8747d16c257a29e70
Bjorn L <Beaverulf@Beaverulf-PC>
2015-03-06 00:14:58 +0100

    Tutorial 8 Complete Spinning Cube

--- 3DGameEngine/src/engineTester/MainGameLoop.java
+++ 3DGameEngine/src/engineTester/MainGameLoop.java
@@ -1,12 +1,18 @@
 package engineTester;
 
-import org.lwjgl.opengl.Display;
+import models.RawModel;
+import models.TexturedModel;
 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import entities.Camera;
+import entities.Entity;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -14,33 +20,109 @@ public class MainGameLoop {
 		
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
+		
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
 		
 		// OpenGL expects vertices to be defined counter clockwise by default.
 		
-		float[] vertices = {
-				// Left bottom triangle
-				-0.5f, 0.5f, 0f,   //v0
-				-0.5f, -0.5f, 0f,  //v1
-				 0.5f, -0.5f, 0f,  //v2
-				 0.5f, 0.5f, 0.0f  //v3	
+		
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
+		};
+		
+		float[] textureCoords = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
 		};
 		
 		int[] indices = {
-				0,1,3, //TOP left (V0, V1, V3)
-				3,1,2 //Buttom right (V3, V1, V2)
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
 		};
 		
-		RawModel model = loader.loadToVAO(vertices, indices);
+		RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("doge2"));
+		TexturedModel staticModel = new TexturedModel(model, texture);
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5), 0, 0, 0, 1);
+		Camera camera = new Camera();
+		
 		
 		while (!Display.isCloseRequested()) {
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			renderer.prepare();
-			//game logic
-			//render
+			
 			shader.start();
-			renderer.render(model);
+			shader.loadViewMatrix(camera);
+			
+			renderer.render(entity,shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
